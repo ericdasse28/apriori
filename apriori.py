ea29a57992c086659403f9c1ae46a9e3002f48e6
Eric Dasse <christ.gououi@gmail.com>
2018-10-27 22:37:41 +0200

    Feat applying apriori and visualizing the results

--- apriori.py
+++ apriori.py
@@ -23,3 +23,30 @@ ncols = store_data.shape[1]
 records = []
 for i in range(nrows):
     records.append([str(store_data.values[i,j]) for j in range(ncols)])
+
+# Applying the apriori
+association_rules = apriori(records, min_support=0.0045, min_confidence=0.2,
+                            min_lift=3, min_length=2)
+association_results = list(association_rules)
+
+# Viewing the results
+print(len(association_results))
+print(association_results[0])
+
+for item in association_rules:
+    
+    # first index of the inner list
+    # Contains base item and add item
+    pair = item[0]
+    items = [x for x in pair]
+    print("Rule: " + items[0] + "->" + items[1])
+    
+    # Second index of the inner list
+    print("Support: "+ str(item[1]))
+    
+    # Third index of the list located at 0th
+    # of the third index of the inner list
+    
+    print("Confidence: " + str(item[2][0][2]))
+    print("Lift: " + str(item[2][0][3]))
+    print("============================================")
