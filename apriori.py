8c49030d057fbb918adb41daf33c4aa811e2727d
Eric Dasse <christ.gououi@gmail.com>
2018-10-27 21:46:06 +0200

    Feat dataset importation and data processing

--- apriori.py
+++ apriori.py
@@ -5,6 +5,21 @@ Created on Sat Oct 27 21:23:41 2018
 @author: deric
 """
 
-import numpy
+# Import the librairies
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas
+from apyori import apriori
+
+
+# Import the dataset
+store_data = pandas.read_csv("../datasets/store_data.csv", header=None)
+print(store_data.head())
+# Useful information
+nrows = store_data.shape[0]
+ncols = store_data.shape[1]
+
+# Data Processing
+records = []
+for i in range(nrows):
+    records.append([str(store_data.values[i,j]) for j in range(ncols)])
